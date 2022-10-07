537e4d7bc8a918041750cdd3c4e3f6e9cc00cc60
Benjamin Groom <benjamin.groom@kcl.ac.uk>
2022-10-07 11:16:13 +0100

    Add home page and half-baked model.

---
+++ things/models.py
@@ -0,0 +1,9 @@
+
+from django.db.models import Model
+
+class Thing(Model):
+
+    def __init__(self, name, description, quantity):
+        self.name = name
+        self.description = description
+        self.quantity = quantity
