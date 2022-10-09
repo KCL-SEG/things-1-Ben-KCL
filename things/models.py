ba26a56e36bdd9234545c66f8cc63b791a05ee16
Benjamin Groom <benjamin.groom@kcl.ac.uk>
2022-10-09 18:51:16 +0100

    Add super call to models.py

--- things/models.py
+++ things/models.py
@@ -4,6 +4,7 @@ from django.db.models import Model
 class Thing(Model):
 
     def __init__(self, name, description, quantity):
+        super().__init__()
         self.name = name
         self.description = description
         self.quantity = quantity
